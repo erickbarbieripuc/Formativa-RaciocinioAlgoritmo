e69b933056908ecc72d0c2eb6253cdc02cf49d79
erickbarbieripuc <167023009+erickbarbieripuc@users.noreply.github.com>
2024-04-23 22:21:02 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+# #Atividade 1
+# lista = []
+# i = 0
+# for i in range(1,11,1):
+#     lista.append(i)
+#     i += 1
+# print(lista)
+
+# #Atividade 2
+# cores = []
+# i=0
+# for i in range(3):
+#     cor = input("Digite a cor:")
+#     cores.append(cor)
+# print(cores[0])
+# print(cores[1])
+# print(cores[2])
+
+# #Atividade 3
+# lista = []
+# i = 0
+# while i < 11:
+#     lista.append(i)
+#     i += 2
+# print(lista)
+
+#Atividade 4
+# lista = []
+# i = 1
+# while i < 11:
+#     lista.append(i)
+#     i += 2
+# print(lista)
+
+# #Atividade 5
+# nomes = ["joao","maria","jose","erick","ana"]
+# nome = input("Digite um nome:")
+# nomes.append(nome)
+# print(nomes)
+
+# #Atividade 6
+# numeros = [1,2,3,4,5]
+# soma = numeros[0] + numeros[1] + numeros[2] + numeros[3] + numeros[4]
+# print(soma)
+
+# #Atividade 7
+# numeros = [1,2,3,4,5]
+# maiorN = max(numeros)
+# print(maiorN)
+
+# #Atividade 8
+# numeros = [1,2,3,4,5]
+# del(numeros[0])
+# print(numeros)
+
+# #Atividade 9
+# numeros = [1,2,3,4,5]
+# del(numeros[4])
+# print(numeros)
+
+# #Atividade 10
+# nomes = ["joao","maria","jose","erick","ana"]
+# n = int(input("Digite o numero do elemento que deseja remover:"))
+# del nomes[n]
+# print(nomes)
+
+
+
+
+
+
+
+
